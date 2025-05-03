957cf82d725f6dc55f7f3128ff3f08c1ebc4f76a
Mohammad-Ehshan <ashmes16@gmail.com>
2025-05-03 22:22:57 +0530

    removing comments

--- app/experience/[id]/page.tsx
+++ app/experience/[id]/page.tsx
@@ -1,27 +1,3 @@
-// import { notFound } from "next/navigation"
-// import ExperienceDetail from "@/components/experience-details"
-// import { getExperienceById } from "@/Data/Index"
-
-// export default function ExperiencePage({ params }: { params: { id: string } }) {
-//   const experienceId = Number.parseInt(params.id)
-//   const experience = getExperienceById(experienceId)
-
-//   if (!experience) {
-//     notFound()
-//   }
-
-//   return (
-//     <main className="min-h-screen bg-black text-white pt-24">
-//       <div className="container mx-auto px-4 py-12">
-//         <ExperienceDetail experience={experience} />
-//       </div>
-//     </main>
-//   )
-// }
-
-
-
-
 import { notFound } from "next/navigation";
 import ExperienceDetail from "@/components/experience-details";
 import { getExperienceById } from "@/Data/Index";
